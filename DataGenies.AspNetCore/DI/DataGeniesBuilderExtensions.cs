fa8ca932960e0c59aa3314e5a121819e4c1ea21f
chesdenis <chesdenis@gmail.com>
2020-01-26 00:18:46 +0300

    initial blank project

---
+++ DataGenies.AspNetCore/DI/DataGeniesBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace DataGenies.AspNetCore.DI
+{
+    public static class DataGeniesBuilderExtensions
+    {
+        public static IApplicationBuilder UseSwagger(
+            this IApplicationBuilder app,
+            Action<DataGeniesOptions> setupAction = null)
+        {
+            var options = app.ApplicationServices.GetService<IOptions<DataGeniesOptions>>()?.Value ?? new DataGeniesOptions();
+            setupAction?.Invoke(options);
+            app.UseMiddleware<DataGeniesMiddleware>(options);
+
+            return app;
+        }
+    }
+
+    public class DataGeniesMiddleware
+    {
+    }
+
+    public class DataGeniesOptions
+    {
+    }
+}
