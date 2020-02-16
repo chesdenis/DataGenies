508a9fc7788fab729901643415770346e7f0c27f
chesdenis <chesdenis@gmail.com>
2020-02-16 04:30:18 +0300

    new idea

--- src/DataGenies.Core/Containers/IContainer.cs
+++ src/DataGenies.Core/Containers/IContainer.cs
@@ -1,15 +1,4 @@
 ﻿namespace DataGenies.Core.Containers
 {
-    public interface IContainer
-    {
-        void Register<T>(object instance)
-            where T : class;
-
-        void Register<T>(object instance, string name)
-            where T : class;
-
-        T Resolve<T>();
-        
-        T Resolve<T>(string name);
-    }
+   
 }
--- src/DataGenies.Core/Extensions/MessageConverters.cs
+++ src/DataGenies.Core/Extensions/MessageConverters.cs
@@ -4,18 +4,5 @@ using System.Text.Json.Serialization;
 
 namespace DataGenies.Core.Extensions
 {
-    public static class MessageExtensions
-    {
-        public static byte[] ToBytes<T>(this T message)
-        {
-            var jsonData = JsonSerializer.Serialize(message);
-            return Encoding.UTF8.GetBytes(jsonData);
-        }
-
-        public static T FromBytes<T>(this byte[] messageAsBytes)
-        {
-            var jsonData = Encoding.UTF8.GetString(messageAsBytes);
-            return JsonSerializer.Deserialize<T>(jsonData);
-        }
-    }
+   
 }
