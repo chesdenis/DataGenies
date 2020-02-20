567d4a4784dc09cf8e1e2c55f4d1c00a9b039c0f
chesdenis <chesdenis@gmail.com>
2020-02-20 21:29:27 +0300

    in memory manager and config entries

---
+++ src/DataGenies.InMemory/BehaviourApplicationInstanceMapping.cs
@@ -0,0 +1,8 @@
+﻿namespace DataGenies.InMemory
+{
+    public class BehaviourApplicationInstanceMapping
+    {
+        public int BehaviourId { get; set; }
+        public int ApplicationInstanceId { get; set; }
+    }
+}
---
+++ src/DataGenies.InMemory/ConfigStoreContent.cs
@@ -0,0 +1,14 @@
+﻿using DataGenies.Core.Models;
+
+namespace DataGenies.InMemory
+{
+    public class ConfigStoreContent
+    {
+        public ApplicationTemplateEntity[] ApplicationTemplates { get; set; }
+        public ApplicationInstanceEntity[] ApplicationInstances { get; set; }
+        public BehaviourTemplateEntity[] BehavioursTemplates { get; set; }
+        public BehaviourInstanceEntity[] BehavioursInstances { get; set; }
+        public BindingEntity[] Bindings { get; set; }
+        public object ApplicationInstancesBehaviours { get; set; }
+    }
+}
