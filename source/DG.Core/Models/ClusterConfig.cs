7130de9321ddd229ceadc5a98d426436264d9631
Vladimir Chirushin <motpfofs@gmail.com>
2020-04-21 17:24:32 +0300

    Add page and forms

--- source/DG.Core/Models/ClusterConfig.cs
+++ source/DG.Core/Models/ClusterConfig.cs
@@ -1,28 +1,30 @@
 ﻿namespace DG.Core.Models
 {
     using System;
+    using System.Collections.Generic;
 
     public class ClusterConfig
     {
         public DateTime LastUpdateTime;
         public string HashMD5;
-        public Node[] Nodes;
-        public ApplicationInstance[] ApplicationInstances;
+        public List<Node> Nodes;
+        public List<ApplicationInstance> ApplicationInstances;
     }
 
-    public class ApplicationInstance
+    public class Node
     {
+        public string NodeName;
         public string HostName;
         public int Port;
         public string HostingModel;
     }
 
-    public class Node
-    {
+    public class ApplicationInstance
+{
         public string Name;
         public string Type;
         public string HostingModel;
-        public string PlacementPolicies;
+        public List<string> PlacementPolicies;
         public int Count;
     }
 }
